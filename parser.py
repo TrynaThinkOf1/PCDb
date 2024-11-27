19031fa8687f3c82a7a2108d18da1621e63c7d80
TrynaThinkOf1 <zeviberlin@gmail.com>
2024-11-27 13:48:28 -0500

    decodes the token successfully

--- parser.py
+++ parser.py
@@ -1,27 +1,36 @@
-import shutil as sh
-import os
-
 tokens = []
 final_token = ""
 
-with open("DATABASES/base1.css", 'r') as css:
-    lines = css.readlines()
-    comment = False
+def parse():
+    global tokens, final_token
+    with open("DATABASES/base1.css", 'r') as css:
+        lines = css.readlines()
+        comment = False
 
-    # parse
-    for line in lines:
-        stripped_line = line.strip()
-        if stripped_line.startswith("/*"):
-            comment = True
-        if not comment:
-            if stripped_line.startswith("color: #"):
-                stripped_line = stripped_line.removeprefix("color: #")
-                stripped_line = stripped_line.removesuffix(";")
-                tokens.append(stripped_line)
-        if stripped_line.endswith("*/"):
-            comment = False
+        # parse
+        for line in lines:
+            stripped_line = line.strip()
+            if stripped_line.startswith("/*"):
+                comment = True
+            if not comment:
+                if stripped_line.startswith("color: #"):
+                    stripped_line = stripped_line.removeprefix("color: #")
+                    stripped_line = stripped_line.removesuffix(";")
+                    tokens.append(stripped_line)
+            if stripped_line.endswith("*/"):
+                comment = False
 
-    for token in tokens:
-        final_token += token
+        for token in tokens:
+            final_token += token
 
-    print(final_token)
+        return final_token
+
+def decode(token):
+    try:
+        byte_data = bytes.fromhex(token)
+        print(byte_data)
+    except ValueError as e:
+        print(e)
+
+if __name__ == "__main__":
+    decode(parse())
